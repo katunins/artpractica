d747ef4ea2e36907378c2b892299ed9fc8b9ddfe
katunins <61758341+katunins@users.noreply.github.com>
2024-07-04 23:46:34 +0300

    add test file

---
+++ AppDelegate.h
@@ -0,0 +1,10 @@
+//#import <React/RCTBridgeDelegate.h>
+//#import <UIKit/UIKit.h>
+#import <RCTAppDelegate.h>
+#import <UserNotifications/UNUserNotificationCenter.h>
+
+
+@interface AppDelegate : RCTAppDelegate <UNUserNotificationCenterDelegate>
+
+
+@end
